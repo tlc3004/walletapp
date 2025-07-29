f260715bd08ddd9bc09ec57552a31616e452aae7
tlc3004 <leonantoniobm@gmail.com>
2025-07-28 19:40:13 -0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}", // <- MUY IMPORTANTE si usas JSX
-  ],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
