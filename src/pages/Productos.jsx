889c54b1d6a9010c233b8a51ff3e217b3adf1806
tlc3004 <leonantoniobm@gmail.com>
2025-07-29 09:15:36 -0500

    commit

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -3,7 +3,7 @@ export default function Productos({ onClose }) {
     <div className="product">
       <div className="content-product">
         <h1 className="titulo">Producto</h1>
-        <img src="/imagenes/donut.png" alt="dibujo" />
+        <img src="/imagenes/donuts.png" alt="dibujo" style={{width:"100vw"}} />
         <button onClick={onClose}>x</button>
       </div>
     </div>
