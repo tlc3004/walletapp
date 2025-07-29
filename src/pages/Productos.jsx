d4be7d042d7bb79a206a2312fb8ba7445095dad3
tlc3004 <leonantoniobm@gmail.com>
2025-07-29 01:13:08 -0500

    fotito

---
+++ src/pages/Productos.jsx
@@ -0,0 +1,10 @@
+
+export default function Productos(){
+    return(
+        <div className="product">
+            <div className="content-product">
+                <h1 className="titulo">producto</h1>
+            </div>
+        </div>
+    )
+}
